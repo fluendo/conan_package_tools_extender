f95a330af3c1f85dcbb24908ff621d38481550cb
Manel J <mjimeno@fluendo.com>
2020-10-04 11:02:29 +0200

    Configuration changes within PyPi

--- setup.py
+++ setup.py
@@ -3,12 +3,17 @@ from distutils.core import setup
 setup(
     name='cpte',
     packages=['cpte'],  # this must be the same as the name above
-    version='0.1.0',
+    version='0.0.1',
     description='Set of classes for rapid development of Conan packages',
     author='Fluendo',
     author_email='mjimeno@fluendo.com',
     url='https://github.com/fluendo/conan_package_tools_extender',  # use the URL to the github repo
     download_url='https://github.com/fluendo/conan_package_tools_extender/tarball/0.1.0',
     keywords=['conan', 'builder'],
-    classifiers=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
